0b938bd0c15cb87a764aa68bcbf825c926dd7c0f
hamilton41 <hamilton032123796@google.com>
2024-08-16 18:31:45 +0800

    feat: 優化select

--- 分類頁sorter/CloudFunction/mysqlService.js
+++ 分類頁sorter/CloudFunction/mysqlService.js
@@ -69,13 +69,13 @@ async function getCategoryProductsById(categoryId) {
             ccp.product_id,
             ccp.position,
             cpe.sku,
-            COALESCE(name_sub.name, '') AS name,
-            COALESCE(mrl_sap_cumulative_qty_sub.mrl_sap_cumulative_qty, '') AS mrl_sap_cumulative_qty,
-            COALESCE(mrl_sap_available_qty_sub.mrl_sap_available_qty, '') AS mrl_sap_available_qty,
-            COALESCE(mrl_sap_status_sub.mrl_sap_status, '') AS mrl_sap_status,
-            COALESCE(mrl_sap_purchase_qty_sub.mrl_sap_purchase_qty, '') AS mrl_sap_purchase_qty,
-            COALESCE(mrl_sap_expected_start_date_sub.mrl_sap_expected_start_date, '') AS mrl_sap_expected_start_date,
-            COALESCE(visibility_sub.visibility, '') AS visibility
+            COALESCE(varchar_attributes.name, '') AS name,
+            COALESCE(varchar_attributes.mrl_sap_cumulative_qty, '') AS mrl_sap_cumulative_qty,
+            COALESCE(varchar_attributes.mrl_sap_available_qty, '') AS mrl_sap_available_qty,
+            COALESCE(varchar_attributes.mrl_sap_status, '') AS mrl_sap_status,
+            COALESCE(text_attributes.mrl_sap_purchase_qty, '') AS mrl_sap_purchase_qty,
+            COALESCE(text_attributes.mrl_sap_expected_start_date, '') AS mrl_sap_expected_start_date,
+            COALESCE(int_attributes.visibility, '') AS visibility
         FROM
             catalog_category_product AS ccp
         JOIN
@@ -84,57 +84,9 @@ async function getCategoryProductsById(categoryId) {
         LEFT JOIN (
             SELECT
                 entity_id,
-                MAX(CASE WHEN attribute_code = 'name' THEN value END) AS name
-            FROM
-                catalog_product_entity_varchar
-            JOIN
-                eav_attribute
-            ON
-                catalog_product_entity_varchar.attribute_id = eav_attribute.attribute_id
-            WHERE
-                eav_attribute.attribute_code = 'name'
-            GROUP BY
-                entity_id
-        ) AS name_sub
-        ON
-            cpe.entity_id = name_sub.entity_id
-        LEFT JOIN (
-            SELECT
-                entity_id,
-                MAX(CASE WHEN attribute_code = 'mrl_sap_cumulative_qty' THEN value END) AS mrl_sap_cumulative_qty
-            FROM
-                catalog_product_entity_varchar
-            JOIN
-                eav_attribute
-            ON
-                catalog_product_entity_varchar.attribute_id = eav_attribute.attribute_id
-            WHERE
-                eav_attribute.attribute_code = 'mrl_sap_cumulative_qty'
-            GROUP BY
-                entity_id
-        ) AS mrl_sap_cumulative_qty_sub
-        ON
-            cpe.entity_id = mrl_sap_cumulative_qty_sub.entity_id
-        LEFT JOIN (
-            SELECT
-                entity_id,
-                MAX(CASE WHEN attribute_code = 'mrl_sap_available_qty' THEN value END) AS mrl_sap_available_qty
-            FROM
-                catalog_product_entity_varchar
-            JOIN
-                eav_attribute
-            ON
-                catalog_product_entity_varchar.attribute_id = eav_attribute.attribute_id
-            WHERE
-                eav_attribute.attribute_code = 'mrl_sap_available_qty'
-            GROUP BY
-                entity_id
-        ) AS mrl_sap_available_qty_sub
-        ON
-            cpe.entity_id = mrl_sap_available_qty_sub.entity_id
-        LEFT JOIN (
-            SELECT
-                entity_id,
+                MAX(CASE WHEN attribute_code = 'name' THEN value END) AS name,
+                MAX(CASE WHEN attribute_code = 'mrl_sap_cumulative_qty' THEN value END) AS mrl_sap_cumulative_qty,
+                MAX(CASE WHEN attribute_code = 'mrl_sap_available_qty' THEN value END) AS mrl_sap_available_qty,
                 MAX(CASE WHEN attribute_code = 'mrl_sap_status' THEN value END) AS mrl_sap_status
             FROM
                 catalog_product_entity_varchar
@@ -143,32 +95,16 @@ async function getCategoryProductsById(categoryId) {
             ON
                 catalog_product_entity_varchar.attribute_id = eav_attribute.attribute_id
             WHERE
-                eav_attribute.attribute_code = 'mrl_sap_status'
+                eav_attribute.attribute_code IN ('name', 'mrl_sap_cumulative_qty', 'mrl_sap_available_qty', 'mrl_sap_status')
             GROUP BY
                 entity_id
-        ) AS mrl_sap_status_sub
+        ) AS varchar_attributes
         ON
-            cpe.entity_id = mrl_sap_status_sub.entity_id
+            cpe.entity_id = varchar_attributes.entity_id
         LEFT JOIN (
             SELECT
                 entity_id,
-                MAX(CASE WHEN attribute_code = 'mrl_sap_purchase_qty' THEN value END) AS mrl_sap_purchase_qty
-            FROM
-                catalog_product_entity_text
-            JOIN
-                eav_attribute
-            ON
-                catalog_product_entity_text.attribute_id = eav_attribute.attribute_id
-            WHERE
-                eav_attribute.attribute_code = 'mrl_sap_purchase_qty'
-            GROUP BY
-                entity_id
-        ) AS mrl_sap_purchase_qty_sub
-        ON
-            cpe.entity_id = mrl_sap_purchase_qty_sub.entity_id
-        LEFT JOIN (
-            SELECT
-                entity_id,
+                MAX(CASE WHEN attribute_code = 'mrl_sap_purchase_qty' THEN value END) AS mrl_sap_purchase_qty,
                 MAX(CASE WHEN attribute_code = 'mrl_sap_expected_start_date' THEN value END) AS mrl_sap_expected_start_date
             FROM
                 catalog_product_entity_text
@@ -177,12 +113,12 @@ async function getCategoryProductsById(categoryId) {
             ON
                 catalog_product_entity_text.attribute_id = eav_attribute.attribute_id
             WHERE
-                eav_attribute.attribute_code = 'mrl_sap_expected_start_date'
+                eav_attribute.attribute_code IN ('mrl_sap_purchase_qty', 'mrl_sap_expected_start_date')
             GROUP BY
                 entity_id
-        ) AS mrl_sap_expected_start_date_sub
+        ) AS text_attributes
         ON
-            cpe.entity_id = mrl_sap_expected_start_date_sub.entity_id
+            cpe.entity_id = text_attributes.entity_id
         LEFT JOIN (
             SELECT
                 entity_id,
@@ -197,13 +133,14 @@ async function getCategoryProductsById(categoryId) {
                 eav_attribute.attribute_code = 'visibility'
             GROUP BY
                 entity_id
-        ) AS visibility_sub
+        ) AS int_attributes
         ON
-            cpe.entity_id = visibility_sub.entity_id
+            cpe.entity_id = int_attributes.entity_id
         WHERE
             ccp.category_id = ${categoryId}
         GROUP BY
             ccp.product_id, cpe.sku;
+
     `;
     return executeQuery(query);
 }
---
+++ 分類頁sorter/CloudFunction/part2-select法.sql
@@ -0,0 +1,81 @@
+SELECT
+    cpe.entity_id,
+    cpe.sku,
+    COALESCE(varchar_attributes.name, '') AS name,
+    COALESCE(varchar_attributes.mrl_sap_cumulative_qty, '') AS mrl_sap_cumulative_qty,
+    COALESCE(varchar_attributes.mrl_sap_available_qty, '') AS mrl_sap_available_qty,
+    COALESCE(varchar_attributes.mrl_sap_status, '') AS mrl_sap_status,
+
+    COALESCE(varchar_attributes.mrl_sap_subcategory, '') AS mrl_sap_subcategory,
+    COALESCE(varchar_attributes.mrl_sap_space, '') AS mrl_sap_space,
+
+    COALESCE(text_attributes.mrl_sap_purchase_qty, '') AS mrl_sap_purchase_qty,
+    COALESCE(text_attributes.mrl_sap_expected_start_date, '') AS mrl_sap_expected_start_date,
+    COALESCE(int_attributes.visibility, '') AS visibility
+FROM
+    catalog_product_entity AS cpe
+LEFT JOIN (
+    SELECT
+        entity_id,
+        MAX(CASE WHEN attribute_code = 'name' THEN value END) AS name,
+        MAX(CASE WHEN attribute_code = 'mrl_sap_cumulative_qty' THEN value END) AS mrl_sap_cumulative_qty,
+        MAX(CASE WHEN attribute_code = 'mrl_sap_available_qty' THEN value END) AS mrl_sap_available_qty,
+        MAX(CASE WHEN attribute_code = 'mrl_sap_status' THEN value END) AS mrl_sap_status,
+        MAX(CASE WHEN attribute_code = 'mrl_sap_subcategory' THEN value END) AS mrl_sap_subcategory,
+        MAX(CASE WHEN attribute_code = 'mrl_sap_space' THEN value END) AS mrl_sap_space
+    FROM
+        catalog_product_entity_varchar
+    JOIN
+        eav_attribute
+    ON
+        catalog_product_entity_varchar.attribute_id = eav_attribute.attribute_id
+    WHERE
+        eav_attribute.attribute_code IN ('name', 'mrl_sap_cumulative_qty', 'mrl_sap_available_qty', 'mrl_sap_status', 'mrl_sap_subcategory', 'mrl_sap_space')
+    GROUP BY
+        entity_id
+) AS varchar_attributes
+ON
+    cpe.entity_id = varchar_attributes.entity_id
+LEFT JOIN (
+    SELECT
+        entity_id,
+        MAX(CASE WHEN attribute_code = 'mrl_sap_purchase_qty' THEN value END) AS mrl_sap_purchase_qty,
+        MAX(CASE WHEN attribute_code = 'mrl_sap_expected_start_date' THEN value END) AS mrl_sap_expected_start_date
+    FROM
+        catalog_product_entity_text
+    JOIN
+        eav_attribute
+    ON
+        catalog_product_entity_text.attribute_id = eav_attribute.attribute_id
+    WHERE
+        eav_attribute.attribute_code IN ('mrl_sap_purchase_qty', 'mrl_sap_expected_start_date')
+    GROUP BY
+        entity_id
+) AS text_attributes
+ON
+    cpe.entity_id = text_attributes.entity_id
+LEFT JOIN (
+    SELECT
+        entity_id,
+        MAX(CASE WHEN attribute_code = 'visibility' THEN value END) AS visibility
+    FROM
+        catalog_product_entity_int
+    JOIN
+        eav_attribute
+    ON
+        catalog_product_entity_int.attribute_id = eav_attribute.attribute_id
+    WHERE
+        eav_attribute.attribute_code = 'visibility'
+    GROUP BY
+        entity_id
+) AS int_attributes
+ON
+    cpe.entity_id = int_attributes.entity_id
+WHERE
+    mrl_sap_status = '常備品'
+AND
+    mrl_sap_space = '客廳'
+AND
+    mrl_sap_subcategory = 'LHF'
+GROUP BY
+    cpe.entity_id, cpe.sku;
