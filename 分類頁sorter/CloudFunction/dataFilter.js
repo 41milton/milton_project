12fe89f5a70dd6e23010b3b488038876891cd835
hamilton41 <hamilton032123796@google.com>
2024-08-21 19:33:58 +0800

    refactor: 優化dataFilter.js的code

--- 分類頁sorter/CloudFunction/dataFilter.js
+++ 分類頁sorter/CloudFunction/dataFilter.js
@@ -6,11 +6,37 @@
 function filterData(data) {
     const { title, items, condition } = data;
     const priceIndex = title.indexOf('折扣後金額');
+    const deliveryDateIndex = title.indexOf('預計交期起算');
+    const quantityIndex = title.indexOf('可銷售數量');
+    const stockIndex = title.indexOf('預計進貨量');
+
 
     if (priceIndex === -1) {
         throw new Error('折扣後金額的標題未找到');
     }
+    if (deliveryDateIndex === -1) {
+        throw new Error('預計交期起算的標題未找到');
+    }
+    if (quantityIndex === -1) {
+        throw new Error('可銷售數量的標題未找到');
+    }
+    if (stockIndex === -1) {
+        throw new Error('預計進貨量的標題未找到');
+    }
 
+
+    const filteredByPrice = filterPriceData(items,condition,priceIndex);
+
+
+
+    return filteredByPrice;
+}
+
+
+
+
+
+function filterPriceData(items,condition,priceIndex){
     const { minPrice = 0, maxPrice = Infinity } = condition.find(cond => cond.name === '折扣後金額') || {};
 
     return items.filter(item => {
